bbc8870840f5c740d9255eab0aeda990a439410f
nassim <bladess@hotmail.fr>
2017-07-25 17:03:16 +0200

    opti

--- projetHtml/app.js
+++ projetHtml/app.js
@@ -1,80 +1,62 @@
 
-$("#buttonForm").click(function(){
-	var nb1 = parseInt($("#input1").val());
-	var nb2 = parseInt($("#input2").val());
-	switch($("#selectOperation").val()){
-		case "+":
-			$("#result").text(nb1 + nb2);
-			break;
-		case "-":
-			$("#result").text(nb1 - nb2);
-			break;
-		case "*":
-			$("#result").text(nb1 * nb2);
-			break;
-		case "/":
-			$("#result").text(nb1 / nb2);
-			break;
-		default:
-			break;
-
-	}
-});
 function main(){
  
 var courant ='';
 var stock ='';
 var imp ='';
 var op ='';
+var operator = ["+","-","*","/"];
 $(".btn-group-vertical").children("button").click(function(){
 	imp = $(this).text();
-	switch(imp){
-		case "+":
-		case "-":
-		case "*":
-		case "/":
+	if(imp=== "=")
+	{
+		$("#result2").text(operate(stock,courant,op));	
+		stock = '';
+		courant ='';
+		op = '';
+	}
+	else{
+		if(operator.indexOf(imp)=== -1)
+		{
+			courant = courant+imp;
+			$("#current").text(courant);	
+		}
+		else{
 			stock = courant;
 			courant ='';
 			op = imp;
 			$("#stock").text(stock);
 			$("#current").text(courant);
 			$("#ope").text(op);
-		break;
-		case "=":
-		switch(op){
-			case "+":
-			$("#result2").text(parseInt(stock) + parseInt(courant));
-			break;
-			case "-":
-			$("#result2").text(parseInt(stock) - parseInt(courant));
-			break;
-			case "*":
-			$("#result2").text(parseInt(stock) * parseInt(courant));
-			break;
-			case "/":
-			$("#result2").text(parseInt(stock) / parseInt(courant));
-			break;
-			default:
-			$("#result2").text("no operator");
-			break;
 		}
-			stock = '';
-			courant ='';
-			op = '';
-		break;
-		default:
-			courant = courant+imp;
-			$("#current").text(courant);
-		break;
-		}
-	
+	}
+});
 
-
+$("#buttonForm").click(function(){
+	var nb1 =$("#input1").val();
+	var nb2 = $("#input2").val();
+	$("#result").text(operate(nb1,nb2,$("#selectOperation").val()));
 });
 
 }
 
-
+function operate(op1, op2, operator){
+	if(operator === "+"){
+		return parseInt(op1) + parseInt(op2)
+	}
+	else if(operator === "-"){
+		return parseInt(op1) - parseInt(op2)
+	}
+	else if(operator === "*"){
+		return parseInt(op1) * parseInt(op2)
+	}
+	else if(operator === "/" && op2 !== '0'){
+		return parseInt(op1) / parseInt(op2)
+	}
+	else{
+		return "error";
+	}
+}
 $(document).ready(function(){
 	main();
 });
